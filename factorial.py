1c47c2c5da39a2b92f07982e80af4096ccf167fd
Safayet Hossain <145759939+safayet9009@users.noreply.github.com>
2024-12-04 00:04:23 +0600

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,10 @@
+s=int(input("enter factorial number:"))
+fact=1
+if s<0:
+ print("factorial doesn't exist")
+elif s==0:
+ print("factorial of 0 is 1")
+else:
+    for i in range(1,s+1):
+     fact=fact*i
+print(fact)
